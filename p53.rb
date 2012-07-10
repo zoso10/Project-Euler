eeed0381b1b269bb4a5b8c395de758832b2a8ad7
Tyler Ewing <tewing10@gmail.com>
2012-07-10 07:21:25 -0400

    Committing anything I have forgotten

--- p53.rb
+++ p53.rb
@@ -15,14 +15,14 @@ end
 
 t1 = Time.now
 (23..100).each do |n|
-	(3..(n-3)).each do |r|
-		if nCr(n, r) > 1000000 then $values += 1 end
+	(3..(n)).each do |r|
+		if nCr(n, r) > 1000000
+			$values += (n - 2 * r + 1)
+			break
+		end
 	end
 end
 t2 = Time.now
 
 puts $values
 print (t2-t1) * 1000, " ms\n"
-
-
-
